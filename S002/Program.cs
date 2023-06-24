fbc5057ac8ec5d82eba8253515510add5d14cfd8
Irina <irina@Mac-mini-Irina.local>
2023-06-24 13:47:29 +0500

    Семинар2

---
+++ S002/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задача №26 Напишите программу, которая принимает на вход число и выдвет количество цифр в чисел
+//  456 -> 3
+//  78 -> 2
+// 89126 -> 5
+
+
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+int digital(int num)
+{
+int count = 0;
+if (num == 0) count = 1;
+else
+{
+while (num != 0)
+{
+num = num / 10;
+count++;
+}
+}
+return count;
+}
+System.Console.WriteLine($"{digital(num)}");
