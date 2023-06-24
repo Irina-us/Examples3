a33c34c8dd93710a816bbee9349f5c04ea3acef4
Irina <irina@Mac-mini-Irina.local>
2023-06-24 13:33:49 +0500

    Семинар1

--- S001/Program.cs
+++ S001/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Clear();
+
+// / Задача 24: Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Summa(int num)
+{
+int result = 0;
+for (int i=1; i<=num; i++)
+result+=i;
+return result;
+}
+
+void Summa2(int num)
+{
+int result = 0;
+for (int i=1; i<=num; i++)
+result+=i;
+System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {result}");
+}
+
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {Summa(num)}");
+Summa2(num);
