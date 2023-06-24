b5e38b798254c12d3168164e8fe47bfdd3d9b72f
Irina <irina@Mac-mini-Irina.local>
2023-06-24 15:14:34 +0500

    ДЗ1

---
+++ hw001/Program.cs
@@ -0,0 +1,17 @@
+﻿// ВСЁ делаем через функции!
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. Math.Pow юзать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+System.Console.WriteLine("Введите целое число1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите целое число2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i=1; i<=num2; i++)
+result = result * num1;
+System.Console.WriteLine($"{num1} в степени {num2} = {result}");
