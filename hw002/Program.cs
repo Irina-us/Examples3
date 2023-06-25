9a04b71e3c29d393256d485d7b26275ae850f753
Irina <irina@Mac-mini-Irina.local>
2023-06-25 09:37:10 +0500

    ДЗ2

---
+++ hw002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Clear();
+// System.Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int result = num%10;
+// int count = 0;
+//    if (num /= 10);
+//    count++;
+
+
+// System.Console.WriteLine($"количество цифр в числе = {count}");
+
+
+
+// System.Console.WriteLine($"Сумма всех чисел в числе {num} равна {Summa(num)}");
+
+// не могу сделать
